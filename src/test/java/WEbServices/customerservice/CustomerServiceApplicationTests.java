4b6b71ca78f106b3b996ed6700fd89c6b2ab0a67
Fhernanda Romo Olea <fheromo@MacBook-Air-de-Fhernanda.local>
2021-06-01 16:13:27 -0500

    First commit

---
+++ src/test/java/WEbServices/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package WEbServices.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
